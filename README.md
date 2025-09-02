cf3d76939596aa0432272046f9f56affbe0dd7be
Rupesh Shrestha <rupeshshrestha2537@gmail.com>
2025-09-02 12:33:21 +1000

    Add portfolio files

---
+++ README.md
@@ -0,0 +1,5 @@
+Portfolio Website
+This repository contains the source code for my personal portfolio website, hosted on GitHub Pages.
+
+Website Link
+👉 Visit My Portfolio
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
+
